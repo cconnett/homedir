3cb0221d82a7082cf26da595cb4f38e4054b2019
Chris Connett <cjc@google.com>
2016-03-15 14:35:14 -0400

    Fix buggy behavior on paths containing spaces in g4s and PROMPT_COMMAND.

--- g4s.bash
+++ g4s.bash
@@ -1,19 +1,21 @@
 function find-parent-google3 {
-  g3=$PWD
-  until [[ $(basename $g3) == "google3" || $g3 == "/" ]]; do
-    g3=$(dirname $g3)
+  g3="$1"
+  shift
+  until [[ $(basename "$g3") == 'google3' || "$g3" == '/' || "$g3" == '.' ]]; do
+    g3=$(dirname "$g3")
+    echo "$g3"
   done
-  if [[ $g3 != "/" ]]; then
-    echo $g3
+  if [[ "$g3" == 'google3' ]]; then
+    echo "$g3"
   fi
 }
 
 function current-switch-target {
-  g3=$(find-parent-google3)
-  if [[ -n $g3 ]]; then
-    target=$(cat ${g3}/../.citc/target_of_switch_client 2> /dev/null)
-    if [[ -n $target ]]; then
-      echo $target
+  g3=$(find-parent-google3 "$PWD")
+  if [[ -n "$g3" ]]; then
+    target=$(cat "${g3}/../.citc/target_of_switch_client" 2> /dev/null)
+    if [[ -n "$target" ]]; then
+      echo "$target"
       return
     fi
   fi
@@ -21,7 +23,7 @@ function current-switch-target {
 
 function g4s {
   G4_SWITCH="g4 switch"
-  if [[ -n $SWITCH_CLIENT ]]; then
+  if [[ -n "$SWITCH_CLIENT" ]]; then
     G4_SWITCH="$G4_SWITCH -s $SWITCH_CLIENT"
   fi
   force=
@@ -29,31 +31,31 @@ function g4s {
   while [[ $# -gt 0 ]]; do
     case "$1" in
       -f)
-        force=$1
+        force="$1"
         shift
         ;;
       *)
-        client=$1
+        client="$1"
         shift
         ;;
     esac
   done
 
   if [[ "$client" == '-' ]]; then
-    client=$(cat ~/.OLDTARGET_${SWITCH_CLIENT})
+    client=$(cat "~/.OLDTARGET_${SWITCH_CLIENT}")
   fi
 
-  prevtarget=$(current-switch-target)
-  echo -n $prevtarget > ~/.OLDTARGET_${SWITCH_CLIENT}
-  output=$($G4_SWITCH $client 2>&1)
-  if [[ $? -ne 0 && -n $force ]]; then
-    g4d -f $client
-    if [[ -n $SWITCH_CLIENT ]]; then
-      g4d $SWITCH_CLIENT
+  prevtarget="$(current-switch-target)"
+  echo -n "$prevtarget" > "~/.OLDTARGET_${SWITCH_CLIENT}"
+  output=$("$G4_SWITCH" "$client" 2>&1)
+  if [[ $? -ne 0 && -n "$force" ]]; then
+    g4d -f "$client"
+    if [[ -n "$SWITCH_CLIENT" ]]; then
+      g4d "$SWITCH_CLIENT"
     fi
-    $G4_SWITCH $client
+    "$G4_SWITCH" "$client"
   else
-    echo $output
+    echo "$output"
   fi
 }
 
